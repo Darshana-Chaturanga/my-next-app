c7538054d6f9d2421f37a3a79496d1c3b09367b3
Darshana-Chaturanga <mpdarshanachaturanga@gmail.com>
2026-07-31 13:18:02 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import HeaderNav from "@/components/header-nav";
+import HeroBanner from "@/components/home/hero-banner";
+
+// SSR - server side render component (Server component)
+export default function Home() {
+  return (
+    <div className="flex flex-col  min-h-screen ">  
+      <HeaderNav/>
+      <main className="flex-1">
+        <HeroBanner/>
+      </main>
+      <footer className="flex h-72 bg-blue-800 ">
+        <div>Footer section</div>
+      </footer>
+    </div>
+  );
+}
---
+++ components/home/hero-banner.jsx
@@ -0,0 +1,11 @@
+
+// Hero banner section
+export default function HeroBanner() {
+  return (
+    <section className="relative overflow-hidden min-h-[70vh] bg-blue-300">
+          <div className="absolute inset-0 z-0  bg-amber-200" >
+
+          </div>
+    </section>
+  )
+}
